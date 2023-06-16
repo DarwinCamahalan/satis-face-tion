5ca01eff8dcea0c29e8bffb6e8b7ca9ba5c9241c
Darwin Camahalan <darpax1@gmail.com>
2023-06-16 19:33:48 +0800

    MY CHANGES

--- main.py
+++ main.py
@@ -498,8 +498,8 @@ def show_data():
 
 def camera_loop():
     global survey_done, start_btn_clicked
-    classifier = load_model(r'C:\Users\User\Desktop\satis-face-tion\ai_trained_model\model.h5')
-    # classifier = load_model(r'D:\CODES\satis-face-tion\ai_trained_model\model.h5')
+    # classifier = load_model(r'C:\Users\User\Desktop\satis-face-tion\ai_trained_model\model.h5')
+    classifier = load_model(r'D:\CODES\satis-face-tion\ai_trained_model\model.h5')
     emotion_labels = ['Unsatisfied', 'Unsatisfied', 'Satisfied', 'Satisfied', 'Unsatisfied', 'Satisfied']
 
     cap = cv2.VideoCapture(0)
@@ -529,8 +529,8 @@ def camera_loop():
         except cv2.error as e:
             print("")
 
-        face_classifier = cv2.CascadeClassifier(r'C:\Users\User\Desktop\satis-face-tion\ai_trained_model\haarcascades\haarcascade_frontalface_default.xml')
-        # face_classifier = cv2.CascadeClassifier(r'D:\CODES\satis-face-tion\ai_trained_model\haarcascades\haarcascade_frontalface_default.xml')
+        # face_classifier = cv2.CascadeClassifier(r'C:\Users\User\Desktop\satis-face-tion\ai_trained_model\haarcascades\haarcascade_frontalface_default.xml')
+        face_classifier = cv2.CascadeClassifier(r'D:\CODES\satis-face-tion\ai_trained_model\haarcascades\haarcascade_frontalface_default.xml')
         faces = face_classifier.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=5)
 
         if len(faces) > 0:
